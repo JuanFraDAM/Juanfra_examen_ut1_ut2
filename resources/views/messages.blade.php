2061255201a2de390bc1a8e2a762e5b6cfdd1ab1
Juan Francisco <juanfranciscorodriguezllarena@alumno.ieselrincon.es>
2024-10-28 14:00:02 +0000

    Examen terminado

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -1,23 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mensajes</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Bootstrap CSS v5.3.3 -->
+    <link rel="stylesheet" href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }}">
 </head>
+
 <body>
-    <div class="container">
-        <h1>Prueba superada</h1>
-        @if($messages->isEmpty())
-            <p>No hay mensajes en la base de datos</p>
-        @else
-            <ul>
-                @foreach($messages as $message)
-                    <li>{{ $message->text }}</li>
-                @endforeach
-            </ul>
-        @endif
-    </div>
+    <main>
+        <div class="vw-100 vh-100 py-2">
+            <div class="w-75 h-100 mx-auto">
+                @if ($messages->isEmpty())
+                    <p>No hay mensajes en la base de datos</p>
+                @else
+                    <table class="table table-hover mh-100">
+                        <caption>Lista de mensajes</caption>
+                        <thead class="table-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col-6">Mensaje</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($messages as $message)
+                                <tr>
+                                    <th scope="row">{{ $message->id }}</th>
+                                    <td>
+                                        @if ($message->black && $message->sub)
+                                            <b><u>{{ $message->text }}</u></b>
+                                        @elseif($message->black)
+                                            <b>{{ $message->text }}</b>
+                                        @elseif($message->sub)
+                                            <u>{{ $message->text }}</u>@else{{ $message->text }}
+                                        @endif
+                                    </td>
+                                    <td>
+                                        {{-- {{ route('doubt.show') }} --}}
+                                        <form class="d-inline" action="{{ route('show.message', $message->id) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <button name="edit" value="{{ $message->id }}" type="submit"
+                                                class="btn btn-info">
+                                                Editar
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @endif
+
+            </div>
+        </div>
+    </main>
 </body>
+<!-- Bootstrap JavaScript Libraries -->
+<script src="{{ asset('bootstrap-5.3.3-dist/js/bootstrap.min.js') }}"></script>
+</script>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\editmessage_controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,6 @@ Route::get('/messages', function () {
     $messages = \App\Models\Message::all();
     return view('messages', ['messages' => $messages]);
 });
+Route::put('/showMessage/{id}', [editmessage_controller::class, 'showMessage'])->name('show.message');
+Route::get('/showMessage/{id}', [editmessage_controller::class, 'showMessage'])->name('show.message');
+Route::put('/editMessage/{id}', [editmessage_controller::class, 'editMessage'])->name('edit.message');
